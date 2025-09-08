87a438b7b51190fac92ed33c35ae16e410456bc4
Rob Brazier <git+github@brzr.co>
2025-09-08 13:03:54 +0100

    use checksum for cachebusting

--- cmd/web/embed.go
+++ cmd/web/embed.go
@@ -1,6 +1,10 @@
 package web
 
-import "embed"
+import (
+	"crypto/sha256"
+	"embed"
+	"fmt"
+)
 
 //go:generate go tool templ generate ./...
 //go:generate npx @tailwindcss/cli -i assets/css/input.css -o static/css/output.css --minify
@@ -8,3 +12,21 @@ import "embed"
 
 //go:embed static/*
 var Static embed.FS
+
+var AssetVersions = func() map[string]string {
+	versions := make(map[string]string)
+	
+	files := []string{
+		"static/css/output.css",
+		"static/js/app.js",
+	}
+	
+	for _, file := range files {
+		if data, err := Static.ReadFile(file); err == nil {
+			hash := sha256.Sum256(data)
+			versions[file] = fmt.Sprintf("%x", hash)[:8]
+		}
+	}
+	
+	return versions
+}()
